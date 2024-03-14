c86ea5359a820f1d8db6428387e369a3910f4d35
Lưu Ka Ka <luukaka2211@gmail.com>
2024-03-14 19:21:25 +0700

    Up deploy sever new11

--- src/components/CallApi.js
+++ src/components/CallApi.js
@@ -1,23 +1,33 @@
-import axios from 'axios';
-
 const BASE_URL = 'http://firstrealestate-001-site1.anytempurl.com/api';
-axios.defaults.withCredentials = true; // Allow sending credentials
-axios.defaults.headers.common['referrerPolicy'] = 'origin-when-cross-origin';
-// A utility function to handle API requests
+
+// Utility function to handle API requests using fetch
 const fetchApiData = async (url) => {
     try {
-        const response = await axios.get(url, {
-            withCredentials: true, // Keep this if you need to send requests without cookies/session data
-           
+        const response = await fetch(url, {
+            method: 'GET', // Specify the method
+            credentials: 'include', // 'include' for cross-origin requests to send cookies
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+            }
         });
-        return response.data;
+
+        // Check if the response is ok (status in the range 200-299)
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        // Parse JSON response
+        const data = await response.json();
+        return data;
     } catch (error) {
         console.error(`Error fetching data from: ${url}`, error);
         return null;
     }
 };
+
 class CallApi {
-    // Your class methods remain unchanged
     static getAllReservations() {
         return fetchApiData(`${BASE_URL}/reservation/GetAllReservation`);
     }
@@ -35,7 +45,6 @@ class CallApi {
     }
 
     static getAllPayMent() {
-        // Since this is an external URL, check if withCredentials is applicable or needed
         return fetchApiData('https://script.googleusercontent.com/macros/echo?user_content_key=JPPSQ-fot0J0W9wj8yXHWkwPtJfFOgDwaki7KoH__NkhHNKlGpJ_H3L_IXiCrPzykz3xWCkiXfCDKQnQPGIdhktQJkVynN4fm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEdV-cB_VcJYh-rThqV7h8GNPA--8n4PWO8ONvoG2LKHDSU-2n4lGV_Y-LbotBxADHIex1tkTLww5QZfCIS2f6d6sJ333kZUNtz9Jw9Md8uu&lib=Me84qEHdJVNIFNFlpsmPH2c5RV3tBGefZ');
     }
 
