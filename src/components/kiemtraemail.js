dfa04361d58cb8c4e93b27acccf2ece80d3edeb0
Lưu Ka Ka <luukaka2211@gmail.com>
2024-03-21 17:02:49 +0700

    Xác thực emailS

--- src/components/kiemtraemail.js
+++ src/components/kiemtraemail.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Testemail() {
   return (
-    <div>Vui lòng kiểm tra email</div>
+    <div style={{ textAlign: 'center', color: 'red' }}>Vui lòng kiểm tra email</div>
   )
 }
